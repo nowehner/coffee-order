9c3e3c9dbd5b9fbd9915628302dc20357fd6be6a
Cole Evans <ce9@iu.edu>
2024-03-08 19:20:01 -0500

    caber push

--- src/main/java/edu/iu/habahram/coffeeorder/controllers/OrderController.java
+++ src/main/java/edu/iu/habahram/coffeeorder/controllers/OrderController.java
@@ -5,13 +5,11 @@ import edu.iu.habahram.coffeeorder.model.Receipt;
 import edu.iu.habahram.coffeeorder.repository.OrderRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
+@CrossOrigin(origins = "http://localhost:8080/")
 public class OrderController {
     private OrderRepository orderRepository;
 
--- src/test/java/edu/iu/habahram/coffeeorder/CoffeeOrderApplicationTests.java
+++ src/test/java/edu/iu/habahram/coffeeorder/CoffeeOrderApplicationTests.java
@@ -1,57 +1,112 @@
 package edu.iu.habahram.coffeeorder;
 
-import edu.iu.habahram.coffeeorder.controllers.OrderController;
-import edu.iu.habahram.coffeeorder.model.OrderData;
-import edu.iu.habahram.coffeeorder.model.Receipt;
-import edu.iu.habahram.coffeeorder.repository.OrderRepository;
+import edu.iu.habahram.coffeeorder.model.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.util.AssertionErrors.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CoffeeOrderApplicationTests {
-	public class OrderControllerTest {
 
-		@Test
-		public void contextLoads() {
-			List<String> coffeeTypes = Arrays.asList("DarkRoast", "HouseBlend", "Decaf", "Espresso");
-			List<String> condimentTypes = Arrays.asList("Mocha", "Milk", "Whip", "Soy");
-
-			OrderRepository orderRepositoryMock = new OrderRepositoryMock();
-			OrderController orderController = new OrderController(orderRepositoryMock);
-
-			for (String coffeeType : coffeeTypes) {
-				for (String condimentType : condimentTypes) {
-					OrderData orderData = new OrderData(coffeeType, List.of(condimentType));
-
-					ResponseEntity<?> responseEntity = orderController.add(orderData);
-
-					assertEquals("", HttpStatus.CREATED, responseEntity.getStatusCode());
-
-					assertTrue(responseEntity.getBody() instanceof Receipt);
-					Receipt receipt = (Receipt) responseEntity.getBody();
-
-					assertNotNull("", receipt.description());
-					assertNotEquals("",0.0f, receipt.cost());
-					assertNotEquals("",0, receipt.id());
-				}
-			}
-		}
-
-		private static class OrderRepositoryMock extends OrderRepository {
-			@Override
-			public Receipt add(OrderData order) {
-				return new Receipt("Mock Order", 10.0f, 123);
-			}
-		}
+	@Test
+	public void testMilkDR() {
+		Beverage beverage = new Milk(new DarkRoast());
+		assertEquals(2.39F, beverage.cost(), 0.01);
+		assertEquals("Dark roast, Milk", beverage.getDescription());
 	}
 
+	@Test
+	public void testMilkHB() {
+		Beverage beverage = new Milk(new HouseBlend());
+		assertEquals(2.05F, beverage.cost(), 0.01);
+		assertEquals("House blend, Milk", beverage.getDescription());
+	}
 
+	@Test
+	public void testMilkDecaf() {
+		Beverage beverage = new Milk(new Decaf());
+		assertEquals(1.68F, beverage.cost(), 0.01);
+		assertEquals("Decaf, Milk", beverage.getDescription());
+	}
+
+	@Test
+	public void testMilkEspresso() {
+		Beverage beverage = new Milk(new Espresso());
+		assertEquals(1.74F, beverage.cost(), 0.01);
+		assertEquals("Espresso, Milk", beverage.getDescription());
+	}
+
+	@Test
+	public void testMochaDR() {
+		Beverage beverage = new Mocha(new DarkRoast());
+		assertEquals(2.29F, beverage.cost(), 0.01);
+		assertEquals("Dark roast, Mocha", beverage.getDescription());
+	}
+	@Test
+	public void testMochaHB() {
+		Beverage beverage = new Mocha(new HouseBlend());
+		assertEquals(1.95F, beverage.cost(),0.01);
+		assertEquals("House blend, Mocha", beverage.getDescription());
+	}
+	@Test
+	public void testMochaDecaf() {
+		Beverage beverage = new Mocha(new Decaf());
+		assertEquals(1.58F, beverage.cost(),0.01);
+		assertEquals("Decaf, Mocha", beverage.getDescription());
+	}
+	@Test
+	public void testMochaEspresso() {
+		Beverage beverage = new Mocha(new Espresso());
+		assertEquals(1.64F, beverage.cost(),0.01);
+		assertEquals("Espresso, Mocha", beverage.getDescription());
+	}
+	@Test
+	public void testSoyDR() {
+		Beverage beverage = new Soy(new DarkRoast());
+		assertEquals(2.79F, beverage.cost(), 0.01);
+		assertEquals("Dark roast, Soy", beverage.getDescription());
+	}
+	@Test
+	public void testSoyHB() {
+		Beverage beverage = new Soy(new HouseBlend());
+		assertEquals(2.45F, beverage.cost(),0.01);
+		assertEquals("House blend, Soy", beverage.getDescription());
+	}
+	@Test
+	public void testSoyDecaf() {
+		Beverage beverage = new Soy(new Decaf());
+		assertEquals(2.08F, beverage.cost(),0.01);
+		assertEquals("Decaf, Soy", beverage.getDescription());
+	}
+	@Test
+	public void testSoyEspresso() {
+		Beverage beverage = new Soy(new Espresso());
+		assertEquals(2.14F, beverage.cost(),0.01);
+		assertEquals("Espresso, Soy", beverage.getDescription());
+	}
+	@Test
+	public void testWhipDR() {
+		Beverage beverage = new Whip(new DarkRoast());
+		assertEquals(2.59F, beverage.cost(), 0.01);
+		assertEquals("Dark roast, Whip", beverage.getDescription());
+	}
+	@Test
+	public void testWhipHB() {
+		Beverage beverage = new Whip(new HouseBlend());
+		assertEquals(2.25F, beverage.cost(),0.01);
+		assertEquals("House blend, Whip", beverage.getDescription());
+	}
+	@Test
+	public void testWhipDecaf() {
+		Beverage beverage = new Whip(new Decaf());
+		assertEquals(1.88F, beverage.cost(),0.01);
+		assertEquals("Decaf, Whip", beverage.getDescription());
+	}
+	@Test
+	public void testWhipEspresso() {
+		Beverage beverage = new Whip(new Espresso());
+		assertEquals(1.94F, beverage.cost(),0.01);
+		assertEquals("Espresso, Whip", beverage.getDescription());
+	}
 }
